0adbadb2ea6ec1ecef805dd2750781ba667393da
sweezing <sweezingmc@gmail.co>
2026-03-31 22:51:31 +0500

    lab5

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig: any = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.isuus24.org",
+};
